602b388a0f14200bcaf9cd09f24ed0f4c02c9cc8
Theles Gmail <theles@gmail.com>
2026-06-21 19:27:07 -0300

    implementando multilingua

--- hello.py
+++ hello.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
+"""Multilanguage Hello World program in Python.
 
-#Este programa imprime "Hello, World!" na tela.
-print("Hello, World!")
+This program will print "Hello World" depending on the locally configured 
+language.
+
+Requirements:
+- LANG variable must be set with a valid language (e.g. "export LANG=pt_BR)
+
+How to run:
+python3 hello.py
+or
+./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Theles Silveira"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+#This program print "Hello, World!" on screen.
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+if current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
